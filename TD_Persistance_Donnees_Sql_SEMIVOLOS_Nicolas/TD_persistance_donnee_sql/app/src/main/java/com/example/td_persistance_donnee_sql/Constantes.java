2940d6f5ca1eaa844bd2f5a140a055936d3f5dbf
NicolasSemivolos <nicolas67000@free.fr>
2021-06-08 11:53:11 +0200

    update
    mauvais fichier commit

--- TD_Persistance_Donnees_Sql_SEMIVOLOS_Nicolas/TD_persistance_donnee_sql/app/src/main/java/com/example/td_persistance_donnee_sql/Constantes.java
+++
@@ -1,8 +0,0 @@
-package com.example.td_persistance_donnee_sql;
-
-public class Constantes {
-    private static final String root_url = "http://192.168.0.18/test/Android/v1/";
-    public static final String url_register = root_url + "userRegister.php";
-
-
-}
